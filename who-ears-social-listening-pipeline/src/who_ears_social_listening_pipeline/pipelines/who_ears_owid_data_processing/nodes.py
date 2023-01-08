82cc47d6c3eb36e434e09ec4f95eb961409c4077
shawnkyzer <shawn.kyzer@throughtworks.com>
2023-01-08 21:22:31 +0100

    Updated headers for readability in doc names with mapping

--- who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/nodes.py
+++ who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/nodes.py
@@ -1,4 +1,44 @@
-"""
-This is a boilerplate pipeline 'who_ears_owid_data_processing'
-generated using Kedro 0.18.4
-"""
+from typing import Any, Callable, Dict
+import pandas as pd
+from datetime import timedelta, datetime as dt
+
+
+def merge_data(partitioned_input: Dict[str, Callable[[], Any]]) -> pd.DataFrame:
+    merged_df = pd.DataFrame()
+    for partition_id, partition_load_func in sorted(partitioned_input.items()):
+        partition_data = partition_load_func()  # Load partition data
+        merged_df = pd.concat([merged_df, partition_data], ignore_index=True, sort=True)
+    return merged_df
+
+
+def update_column_names_from_mapping(who_ears_doc_mapping, merged_daily_who_ears):
+    from re import sub
+
+    def to_snake_case(s):
+        return '_'.join(sub('([A-Z][a-z]+)', r' \1',
+                            sub('([A-Z]+)', r' \1',
+                                s.replace('-', ' '))).split()).lower()
+
+    who_ears_doc_mapping['document_name'] = who_ears_doc_mapping['document_name'].apply(to_snake_case)
+
+    for col_name in merged_daily_who_ears.columns:
+        # Extract the index from the column name
+        if len(col_name.split('-')) == 4:
+            index = int(col_name.split('-')[3])
+            suffix = '_' + col_name.split('-')[1]
+            new_name = who_ears_doc_mapping.loc[who_ears_doc_mapping['doc_index'] == index, 'document_name'][index-1]
+            merged_daily_who_ears.rename(columns={col_name: new_name + suffix}, inplace=True)
+        elif len(col_name.split('-')) > 2:
+            index = int(col_name.split('-')[2])
+            suffix = '_' + col_name.split('-')[1]
+            new_name = who_ears_doc_mapping.loc[who_ears_doc_mapping['doc_index'] == index, 'document_name'][index-1]
+            merged_daily_who_ears.rename(columns={col_name: new_name + suffix}, inplace=True)
+        elif len(col_name.split('-')) > 1:
+            index = int(col_name.split('-')[1])
+            suffix = ""
+            new_name = who_ears_doc_mapping.loc[who_ears_doc_mapping['doc_index'] == index, 'document_name'][index-1]
+            merged_daily_who_ears.rename(columns={col_name: new_name + suffix}, inplace=True)
+        else:
+            continue
+
+    return merged_daily_who_ears
--- who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/pipeline.py
+++ who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/pipeline.py
@@ -1,10 +1,19 @@
-"""
-This is a boilerplate pipeline 'who_ears_owid_data_processing'
-generated using Kedro 0.18.4
-"""
-
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import merge_data, update_column_names_from_mapping
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=merge_data,
+            inputs="raw_daily_who_ears",
+            outputs="merged_daily_who_ears",
+            name="merge_who_ears_partitions",
+        ),
+        node(
+            func=update_column_names_from_mapping,
+            inputs=["who_ears_doc_mapping", "merged_daily_who_ears"],
+            outputs="processed_daily_who_ears",
+            name="update_column_names_from_mapping",
+        ),
+    ])

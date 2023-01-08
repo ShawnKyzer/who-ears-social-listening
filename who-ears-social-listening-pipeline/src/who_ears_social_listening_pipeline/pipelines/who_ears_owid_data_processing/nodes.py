22739fefcae21fcb1d3d339f75900aafd21c3ac2
shawnkyzer <shawn.kyzer@throughtworks.com>
2023-01-08 08:48:01 +0100

    update dependencies to add kedro viz and create the initial stucture for data pipeline

---
+++ who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'who_ears_owid_data_processing'
+generated using Kedro 0.18.4
+"""
---
+++ who-ears-social-listening-pipeline/src/who_ears_social_listening_pipeline/pipelines/who_ears_owid_data_processing/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'who_ears_owid_data_processing'
+generated using Kedro 0.18.4
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
